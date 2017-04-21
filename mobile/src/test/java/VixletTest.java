0b52fbf0002582876246a15408049f5e4743e43e
Frank Zhao <frank.zhao@vixlet.com>
2017-04-21 15:47:48 -0700

    test automation infrastructure : II

--- mobile/src/test/java/VixletTest.java
+++ mobile/src/test/java/VixletTest.java
@@ -6,18 +6,17 @@ import org.openqa.selenium.By;
 
 public class VixletTest extends AppiumTest {
 
-	@Category({SignUp.class, Android_only.class}) 
+    @Category({SignUp.class, Android_only.class}) 
     @Test
     public void appLoads() {
         VixletWelcomePage welcomePage = new VixletWelcomePage(driver, platform).waitUntilLoaded();
     }
 	
-	@Category({SignUp.class, Android_only.class}) 
+    @Category({SignUp.class, Android_only.class}) 
     @Test
     public void getStarted() {
         VixletWelcomePage wp = new VixletWelcomePage(driver, platform).waitUntilLoaded();
         wp.getStarted();
     }
-
 
 }
